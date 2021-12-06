4ccde866b69a0134b8817c9d4adc51c1c93d4fe1
Winicius Cabral <77806133+winiciuscabral@users.noreply.github.com>
2021-12-06 19:06:30 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -8,16 +8,37 @@ $(function() {
         nextArrow: $(".arrow-next"),
         responsive: [
             {
-                breakpoint: 640,
+                breakpoint: 1200,
                 settings: {
-                    slidesToShow: 1,
+                  slidesToShow: 3,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 760,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
+                
+            /*
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 4,
                     slidesToScroll: 1
                 }   
             }
         ]
-    });
-})
-
-// prevArrow: $(".arrow-prev"), 
-        //nextArrow: $(".arrow-next"),
-
+    }); */
+}) 
